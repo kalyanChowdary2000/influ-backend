3df2dd0d8205f7c8e3992157c81c29f0daf12a02
kalyanChowdary2000 <181fa04376@gmail.com>
2023-10-30 16:12:20 +0530

    chnages

--- src/classes/instagram.ts
+++ src/classes/instagram.ts
@@ -25,26 +25,30 @@ export class Instagram {
         }
     }
     static async updateInstagramData() {
-        console.log("updating instagram after 30 min from now ", new Date().toLocaleDateString(), new Date().toLocaleTimeString());
-        setInterval(async () => {
-            let data: any = await MongoConnection.findAllUsers();
-            let userData = data.data
-            for (let i = 0; i < userData.length; i++) {
-                let instagramId = userData[i].instagram;
-                let axiosResponse = await axios.post('http://127.0.0.1:6061/get_engagement_rate', {
-                    username: instagramId
-                })
-                console.log(axiosResponse.data)
-                let followerCount = axiosResponse.data.followers_count;
-                let engagementRate = axiosResponse.data.engagement_rate;
-                let reach = followerCount * engagementRate;
-                await MongoConnection.updateInstagram(instagramId, {
-                    followerCount: followerCount,
-                    engagementRate: engagementRate,
-                    reach: reach
-                })
-            }
-        }, 1000 * 60 * 30);
+        try {
+            console.log("updating instagram after 30 min from now ", new Date().toLocaleDateString(), new Date().toLocaleTimeString());
+            setInterval(async () => {
+                let data: any = await MongoConnection.findAllUsers();
+                let userData = data.data
+                for (let i = 0; i < userData.length; i++) {
+                    let instagramId = userData[i].instagram;
+                    let axiosResponse = await axios.post('http://127.0.0.1:6061/get_engagement_rate', {
+                        username: instagramId
+                    })
+                    console.log(axiosResponse.data)
+                    let followerCount = axiosResponse.data.followers_count;
+                    let engagementRate = axiosResponse.data.engagement_rate;
+                    let reach = followerCount * engagementRate;
+                    await MongoConnection.updateInstagram(instagramId, {
+                        followerCount: followerCount,
+                        engagementRate: engagementRate,
+                        reach: reach
+                    })
+                }
+            }, 1000 * 60 * 30);
+        } catch (e: any) {
+            console.log(e)
+        }
     }
 }
 
--- src/classes/youtube.ts
+++ src/classes/youtube.ts
@@ -28,26 +28,30 @@ export class Youtube {
         }
     }
     static async updateYoutubeData() {
-        console.log("updating Youtube after 30 min from now ", new Date().toLocaleDateString(),new Date().toLocaleTimeString());
-        setInterval(async () => {
-            let data: any = await MongoConnection.findAllUsers();
-            let userData = data.data
-            for (let i = 0; i < userData.length; i++) {
-                let channelId = userData[i].youtube;
-                let axiosResponse = await axios.post('http://127.0.0.1:6061/get_yt_engagement_rate', {
-                    channelId: channelId,
-                    developerKey: "AIzaSyDpX8JwD0z73piVfiTYYq-g0a6LvFqBMVs"
-                });
-                console.log(axiosResponse.data)
-                let followerCount = axiosResponse.data.subscriberCount;
-                let engagementRate = axiosResponse.data.engagementRate;
-                let reach = followerCount * engagementRate;
-                await MongoConnection.updateYoutube(channelId, {
-                    followerCount: followerCount,
-                    engagementRate: engagementRate,
-                    reach: reach
-                })
-            }
-        }, 1000 * 60 * 30);
+        try {
+            console.log("updating Youtube after 30 min from now ", new Date().toLocaleDateString(), new Date().toLocaleTimeString());
+            setInterval(async () => {
+                let data: any = await MongoConnection.findAllUsers();
+                let userData = data.data
+                for (let i = 0; i < userData.length; i++) {
+                    let channelId = userData[i].youtube;
+                    let axiosResponse = await axios.post('http://127.0.0.1:6061/get_yt_engagement_rate', {
+                        channelId: channelId,
+                        developerKey: "AIzaSyDpX8JwD0z73piVfiTYYq-g0a6LvFqBMVs"
+                    });
+                    console.log(axiosResponse.data)
+                    let followerCount = axiosResponse.data.subscriberCount;
+                    let engagementRate = axiosResponse.data.engagementRate;
+                    let reach = followerCount * engagementRate;
+                    await MongoConnection.updateYoutube(channelId, {
+                        followerCount: followerCount,
+                        engagementRate: engagementRate,
+                        reach: reach
+                    })
+                }
+            }, 1000 * 60 * 30);
+        } catch (e: any) {
+            console.log(e);
+        }
     }
 }

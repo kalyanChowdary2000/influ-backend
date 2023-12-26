1afefe602e2c79a850ab5fae3c5d4f5373088b8d
kalyanChowdary2000 <181fa04376@gmail.com>
2023-12-26 10:49:55 +0530

    changes

--- src/actions/transaction/decrypt.ts
+++ src/actions/transaction/decrypt.ts
@@ -12,21 +12,17 @@ const ccavenue = new Configure({
 })
 router.get("/", async (req: any, res: any) => {
     try {
-        try {
             console.log(req.body["encResp"])
             let decryptedData = ccavenue.decrypt(req.body["encResp"])
             console.log("----------------------->>> decrypted data", decryptedData)
             res.send({ "success": true, data: decryptedData });
-        } catch (e: any) {
-            console.log(e);
-            res.send({ "success": false, error: e.toString() });
-        }
+      
     } catch (e: any) {
         console.log(e)
-        res.status(400).send(await Encrypt.jsonEncrypt({
+        res.status(400).send({
             success: false,
             message: `This is an error! ${e}`
-        }));
+    });
     }
 })
 export default router;
